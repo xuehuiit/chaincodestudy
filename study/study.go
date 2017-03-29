e543c6e7bc8460f8e08c6405529ac6d4ca9cf6ce
fx19800215@163.com <Fx19800215>
2017-03-29 16:09:37 +0800

    11

--- study/study.go
+++ study/study.go
@@ -1,86 +1,1 @@
-package main
-
-import (
-	"errors"
-	"fmt"
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"strconv"
-)
-
-// 数据结构
-type SimpleChaincode struct {
-	// 初始化方法，键值对的简单存储
-}
-
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	var A string // Entity
-	var Aval int // Asset holding
-	var err error
-
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2")
-	}
-
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d\n", Aval)
-
-	// Write the state to the ledger - this put is legal within Run
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
-}
-
-// Invoke is a no-op
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	return nil, nil
-}
-
-// 查询接口
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-
-	if function != "query" {
-		return nil, errors.New("Invalid query function name. Expecting \"query\"")
-	}
-	var A string // Entity
-	var Aval int // Asset holding
-	var err error
-
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2")
-	}
-
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d\n", Aval)
-
-	// Write the state to the ledger - this put is illegal within Run
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		jsonResp := "{\"Error\": \"  ##### hello fabric ######   !Cannot put state within chaincode query\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	fmt.Printf("Something is wrong. This query should not have succeeded")
-	return nil, nil
-
-}
-
-func main() {
-
-	err := shim.Start(new(SimpleChaincode))
-	if err != nil {
-		fmt.Printf("Error starting chaincode: %s", err)
-	}
-
-}
+package mainimport (	"errors"	"fmt"	"github.com/hyperledger/fabric/core/chaincode/shim"	"strconv"	"time")// 数据结构type SimpleChaincode struct {	// 初始化方法，键值对的简单存储}func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {	var A string // Entity	var Aval int // Asset holding	var err error	if len(args) != 2 {		return nil, errors.New("Incorrect number of arguments. Expecting 2")	}	// Initialize the chaincode	A = args[0]	Aval, err = strconv.Atoi(args[1])	if err != nil {		return nil, errors.New("Expecting integer value for asset holding")	}	fmt.Printf("Aval = %d\n", Aval)	// Write the state to the ledger - this put is legal within Run	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))	if err != nil {		return nil, err	}	return nil, nil}// Invoke is a no-opfunc (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {	return nil, nil}// 查询接口func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {	if function != "query" {		return nil, errors.New("Invalid query function name. Expecting \"query\"")	}	var A string // Entity	var Aval int // Asset holding	var err error	if len(args) != 2 {		return nil, errors.New("Incorrect number of arguments. Expecting 2")	}	A = args[0]	Aval, err = strconv.Atoi(args[1])	if err != nil {		return nil, errors.New("Expecting integer value for asset holding")	}	fmt.Printf("Aval = %d\n", Aval)	// Write the state to the ledger - this put is illegal within Run	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))	datetime :=time.Now().Format("2006-01-02 15:04:05")	if err != nil {		jsonResp := "{\"Error\": \"  ##### hello fabric ######   !Cannot put state within chaincode query\",\"time\":\"}"+datetime		return nil, errors.New(jsonResp)	}	fmt.Printf("Something is wrong. This query should not have succeeded")	return nil, nil}func main() {	err := shim.Start(new(SimpleChaincode))	if err != nil {		fmt.Printf("Error starting chaincode: %s", err)	}}
