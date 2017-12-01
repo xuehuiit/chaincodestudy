4f130ee0494d8ac8d08e89e42254e07ce1db70f9
robertfeng <fx19800215@163.com>
2017-12-01 23:19:59 +0800

    m

--- simpledemo/simplechaincode_test.go
+++ simpledemo/simplechaincode_test.go
@@ -14,11 +14,17 @@ import (
 	//"fmt"
 	"testing"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"fmt"
 )
 
 
 func checkInit(t *testing.T, stub *shim.MockStub, args [][]byte){
 
+	res := stub.MockInit("1", args)
+	if res.Status != shim.OK {
+		fmt.Println("Init failed", string(res.Message))
+		t.FailNow()
+	}
 }
 
 
